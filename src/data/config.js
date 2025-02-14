c01383edfa7f06435efc8f5bf606973d7da7a0d6
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-02-14 17:22:12 -0300

    Update config.js

--- src/data/config.js
+++ src/data/config.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const oracledb = require("oracledb");
 
-async function getConnection() {
-    return await oracledb.getConnection({
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        connectString: process.env.DB_CONNECTIONSTRING,
-    });
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    connectString: process.env.DB_CONNECTIONSTRING,
 }
+
+oracledb.initOracleClient({ libDir: '' });
+
+async function getConnection() {
+    try {
+        const connection= await oracledb.getConnection(config);
+        console.log('Banco de dados Oracle conectado com sucesso!');
+        return connection;
+    } catch (err) {
+        console.error('Erro ao conectar com o banco de dados Oracle: ', err);
+        throw err;
+    }
+};
 
 module.exports = { getConnection };
