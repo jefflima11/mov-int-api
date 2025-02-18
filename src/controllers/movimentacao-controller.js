437714cfb457c75b6424d46b29e3d5962dc1aabe
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-02-18 16:50:42 -0300

    Update movimentacao-controller.js

--- src/controllers/movimentacao-controller.js
+++ src/controllers/movimentacao-controller.js
@@ -29,47 +29,90 @@ const getMovId = async (req, res) => {
 };
 
 const postMovId = async (req, res) => {
+    let connection;
+
     try {
-        const connection = await connectOracle();
+        connection = await connectOracle();
 
-        const consulta = await connection.execute(
-            `SELECT * FROM DBAMV.LOG_MOV_INT_HUMS WHERE SN_IMPORTADO = 'N' AND ROWNUM <= 2`
+        const atendimento = req.body.atendimento;
+
+        // Obter o cdAtendimento e cdLeito
+        const dadosDaMovimentacao = await connection.execute(
+            `SELECT CD_ATENDIMENTO, CD_LEITO
+             FROM DBAMV.LOG_MOV_INT_HUMS
+             WHERE ID_LOG_MOV_INT = (
+                 SELECT MAX(ID_LOG_MOV_INT) 
+                 FROM DBAMV.LOG_MOV_INT_HUMS 
+                 WHERE SN_IMPORTADO = 'N' AND CD_ATENDIMENTO =  :atendimento
+             )`,{ atendimento }
         );
-        
-        await connection.close();
-        
-        res.status(200).json({ message: `Movimentação inserida com sucesso `, data: consulta.metaData.map(col => col.name)});
 
-        // const { setor, empresa, atendimento, msg } = req.body;
+        if (dadosDaMovimentacao.rows.length === 0) {
+            return res.status(404).json({ message: "Nenhum atendimento encontrado." });
+        }
 
-        // const result = await connection.execute(
-        //     `SELECT SEQ_PT_MVTO_PACIENTE.NEXTVAL FROM DUAL`
-        //   );
+        const [cdAtendimento, cdLeito] = dadosDaMovimentacao.rows[0];
 
-        // const nextPaciente = result.rows[0][0]; 
+        // Obter o cdPaciente
+        const dadosDoPaciente = await connection.execute(
+            `SELECT CD_PACIENTE FROM DBAMV.ATENDIME WHERE CD_ATENDIMENTO = :cdAtendimento`,
+            { cdAtendimento }
+        );
 
-        // await connection.execute(
-        //     `
-        //     BEGIN
-        //         DBAMV.SP_INSERT_PT_MVTO_PACIENTE(
-        //             :nextPaciente,
-        //             :nextPaciente,
-        //             :setor,
-        //             :empresa,
-        //             :atendimento,
-        //             :msg                    
-        //         );
-        //     END;
-        //     `
-        //     , 
-        //     { nextPaciente, setor, empresa, atendimento, msg},
-        //     { autoCommit: true }
-        //     );
-        
-        // await connection.commit();
+        if (dadosDoPaciente.rows.length === 0) {
+            return res.status(404).json({ message: "Paciente não encontrado." });
+        }
+
+        const cdPaciente = dadosDoPaciente.rows[0][0];
+
+        // Obter o cdSetor
+        const dadosDoLeito = await connection.execute(
+            `SELECT UNID_INT.CD_SETOR
+             FROM DBAMV.LEITO
+             INNER JOIN DBAMV.UNID_INT ON LEITO.CD_UNID_INT = UNID_INT.CD_UNID_INT
+             WHERE CD_LEITO = :cdLeito`,
+            { cdLeito }
+        );
+
+        if (dadosDoLeito.rows.length === 0) {
+            return res.status(404).json({ message: "Leito não encontrado." });
+        }
+
+        const cdSetor = dadosDoLeito.rows[0][0];
+
+        const pegaSequenciaDoPaciente = await connection.execute(
+           `SELECT SEQ_PT_MVTO_PACIENTE.NEXTVAL FROM DUAL`
+          );
+
+        const nextPaciente = pegaSequenciaDoPaciente.rows[0][0];
+
+        await connection.execute(
+            `BEGIN 
+                DBAMV.SP_INSERT_PT_MVTO_PACIENTE(
+                    :nextPaciente,
+                    :cdPaciente,
+                    :cdSetor,
+                    1,
+                    :cdAtendimento,
+                    'Teste 02'
+            );  
+            END;`,
+            { nextPaciente, cdPaciente, cdSetor, cdAtendimento },
+            { autoCommit: true }
+        );
+
+        res.status(200).json({ message: 'Movimentação inserida com sucesso' });
+
     } catch (error) {
-        res.status(500).json({ message: 'Error on postMovId', error });
+        console.error("Erro no postMovId:", error);
+        res.status(500).json({ message: 'Erro ao buscar os dados', error });
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
     }
-}
+};
+
+
 
 module.exports = { getMov, getMovId, postMovId};
