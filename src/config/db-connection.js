4d0900595545b588a0cdede0a35b0c0a4ac265ec
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-02-20 15:45:38 -0300

    Update db-connection.js

--- src/config/db-connection.js
+++ src/config/db-connection.js
@@ -12,7 +12,6 @@ oracledb.initOracleClient({ libDir: '' });
 async function connectOracle() {
   try {
     const connection = await oracledb.getConnection(config);
-    console.log('Banco de dados Oracle conectado');
     return connection;
   } catch (error) {
     console.error('Erro ao conectar o banco de dados', error);
